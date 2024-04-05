1db6f974b85a564488e7911a7ba469254b9ae09b
andrew-walthall <acw44@students.uwf.edu>
2024-04-05 08:19:59 -0500

    modified tests

--- src/app/controllers/app.controller.spec.ts
+++ src/app/controllers/app.controller.spec.ts
@@ -5,35 +5,31 @@ import { AppController } from "./app.controller";
 import { HandlebarsMiddleware } from '../middleware/handlebars.middleware';
 import { UserService } from "../services/user.service";
 
-
 describe("AppController", () => {
   let app: Application;
   let controller: AppController;
 
   // Run this code before every test
   beforeAll(() => {
-  // Create an express instance for testing
-  app = express();
+    // Create an express instance for testing
+    app = express();
 
-  // Set up handlebars for our templating
-  HandlebarsMiddleware.setup(app);
+    // Set up handlebars for our templating
+    HandlebarsMiddleware.setup(app);
 
-  // Our userService instance to pass to the controller
-  let userService = new UserService();
+    // Our userService instance to pass to the controller
+    let userService = new UserService();
 
-  // Our controller instance to test
-  controller = new AppController(userService);
+    // Our controller instance to test
+    controller = new AppController(userService);
 
-  // Load the controller's router for testing
-  app.use(controller.router);
-});
+    // Load the controller's router for testing
+    app.use(controller.router);
+  });
 
-  it("should return a welcome", async () => {
-    return request(app)
-      .get("/")
-      .then((res) => {
-        expect(res.statusCode).toEqual(200);
-        expect(res.text).toMatch(/Welcome to My Website/);
-      });
+  it('should render the login page', async () => {
+    const res = await request(app).get('/login');
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toContain('<form method="POST" action="/processLogin">');
   });
 });
--- src/app/controllers/app.controller.ts
+++ src/app/controllers/app.controller.ts
@@ -1,6 +1,6 @@
-import {Response, Router} from "express";
-import {pino} from "pino";
-import {UserService} from "../services/user.service";
+import { Request, Response, Router } from "express";
+import pino from "pino";
+import { UserService } from "../services/user.service";
 
 export class AppController {
   public router: Router = Router();
@@ -25,29 +25,33 @@ export class AppController {
     });
 
     this.router.post("/signup", async (req: any, res) => {
-      req.session.user = await this.userService.createUser(req.body.username, req.body.email, req.body.password);
-      res.redirect("/");
-    });
-
-    // Handle login form submissions
-    this.router.post("/processLogin", async (req: any, res) => {
-      const user = await this.userService.authenticateUser(req.body.username, req.body.password);
-      if (user) {
-        req.session.user = user;
+      try {
+        req.session.user = await this.userService.createUser(req.body.username, req.body.email, req.body.password);
         res.redirect("/");
-      } else {
-        res.status(401).send("Invalid username or password");
+      } catch (error) {
+        this.log.error(error);
+        res.status(500).send('An error occurred');
       }
     });
 
-    // Enforce security
+    this.router.post("/processLogin", async (req: any, res) => {
+      try {
+        const user = await this.userService.authenticateUser(req.body.username, req.body.password);
+        if (user) {
+          req.session.user = user;
+          res.redirect("/");
+        } else {
+          res.status(401).send("Invalid username or password");
+        }
+      } catch (error) {
+        this.log.error(error);
+        res.status(500).send('An error occurred');
+      }
+    });
+
     this.router.use((req: any, res, next) => {
-      // If the user is set in the session,
-      // pass them on
       if (req.session.user) {
         next();
-
-        // Otherwise, send them to the login page
       } else {
         res.render("login", {
           error: "You need to log in first",
@@ -55,14 +59,14 @@ export class AppController {
       }
     });
 
-    // Serve the home page
     this.router.get("/", (req: any, res: Response) => {
       try {
         res.render("home", {
           user: req.session.user,
         });
-      } catch (err) {
-        this.log.error(err);
+      } catch (error) {
+        this.log.error(error);
+        res.status(500).send('An error occurred');
       }
     });
   }
